77d54c54cb4feebff79a83969aadab6a3bde50f1
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-05-25 10:15:51 +0200

    unused method from main removed

--- src/main/java/edu/tseidler/Main.java
+++ src/main/java/edu/tseidler/Main.java
@@ -1,21 +1,13 @@
 package edu.tseidler;
 
 import edu.tseidler.hanoi.Hanoi;
-import edu.tseidler.hanoi.Move;
 
 public class Main {
     public static void main(String[] args) {
-        Hanoi hanoi = new Hanoi(30);
+        Hanoi hanoi = new Hanoi(10);
         System.out.println("solution of the problem:");
         System.out.println(hanoi);
         hanoi.solve();
         System.out.println("finished");
-    }
-
-    private static void applyMoveAndPresent(Hanoi hanoi, Move m) {
-        System.out.println(m);
-        System.out.println();
-        hanoi.applyMoveToBoard(m);
-        System.out.println(hanoi);
     }
 }
